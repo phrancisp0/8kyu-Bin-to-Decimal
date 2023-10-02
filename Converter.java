8159efb010dee48e1d327c2141c02554c350ed70
Francis Patawah <phrancisp0@gmail.com>
2023-10-01 22:58:13 -0400

    bin to decimal 8kyu

---
+++ Converter.java
@@ -0,0 +1,10 @@
+public class Converter{
+  public static int binToDecimal(String inp){
+    return Integer.parseInt(inp, 2);
+  }
+}
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+*/
